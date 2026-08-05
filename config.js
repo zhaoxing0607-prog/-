ad179561d2d2ebb2600d0861994ccad41e3d2846
XING ZHAO <zhaoxing0607-prog@users.noreply.github.com>
2026-08-06 00:15:04 +0200

    config: connect Supabase workspace

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Configuration Supabase. Copiez vos valeurs depuis Project Settings > API.
 // La clé publique (publishable/anon) peut être utilisée dans le navigateur.
 window.MOLDFLOW_CONFIG = {
-  supabaseUrl: '',
-  supabaseKey: '',
+  supabaseUrl: 'https://wvzckecdrcsfiygytxfv.supabase.co',
+  supabaseKey: 'sb_publishable_t7wtKND19IR_3qZsnk4utg_IauXSLIo',
   workspaceKey: 'expedit'
 };
